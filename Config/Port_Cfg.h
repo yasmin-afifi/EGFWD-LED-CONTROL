f45e1ce27f69205a3eb3d2c77e33e8abe72a6af8
yasmin afifi <67705548+yasmin-afifi@users.noreply.github.com>
2022-09-17 18:20:34 +0200

    Uploading Config files

---
+++ Config/Port_Cfg.h
@@ -0,0 +1,43 @@
+/**********************************************************************************************************************
+ *  FILE DESCRIPTION
+ *  -------------------------------------------------------------------------------------------------------------------
+ *         File:  Port_Cfg.h
+ *       Module:  Port
+ *
+ *  Description:  Header file for TM4C123GH6PM Microcontroller - Port Driver Config   
+ *  
+ *********************************************************************************************************************/
+#ifndef PORT_CFG_H
+#define PORT_CFG_H
+
+
+
+
+
+
+
+/**********************************************************************************************************************
+ *  GLOBAL CONSTANT MACROS
+ *********************************************************************************************************************/
+#define NO_OF_CHANNLES 2
+
+#define PortConf_PF0	     (uint8)0
+#define PortConf_PF1       (uint8)1
+
+/* Tiva C Port ID's  */
+#define PortConf_PORTA_PORT_NUM                 (uint8)0 
+#define PortConf_PORTB_PORT_NUM                 (uint8)1 
+#define PortConf_PORTC_PORT_NUM                 (uint8)2 
+#define PortConf_PORTD_PORT_NUM                 (uint8)3 
+#define PortConf_PORTE_PORT_NUM                 (uint8)4 
+#define PortConf_PORTF_PORT_NUM                 (uint8)5 
+
+
+
+
+
+#endif  /* PORT_CFG_H */
+
+/**********************************************************************************************************************
+ *  END OF FILE: Port_Cfg.h
+ *********************************************************************************************************************/
